150c7c5a5aa7d1163a91f0b190a5022d0298b39c
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2022-04-18 19:23:45 +0100

    init

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,67 +1,8 @@
-import React from 'react';
-import './Navbar.css'
-import  {
-  Nav,
-  NavLink,
-  Bars,
-  NavMenu,
-  NavBtn,
-  NavBtnLink
-}  from   './NavbarElement.js';
-import {InputGroup , DropdownButton ,Dropdown,Form , FormControl} from  'react-bootstrap';
-import { FaSearch } from 'react-icons/fa';
+import React from 'react'
 
 function Navbar() {
   return (
-  <>
-       <Nav>
-         <NavLink  to="/">
-           <h1>LOGO</h1>
-
-           <div>
-           <form className="example" >
-     <input type="text" placeholder="Search.." name="search"/>
-      <button type="submit"><FaSearch/></button>
-             </form>
-             </div>
-           </NavLink>
-          
-
-           <Bars/>
-           <NavMenu>
-
-          
-           <NavLink to ="/about" activeStyle>
-            Home
-
-           </NavLink>
-           <NavLink to ="/about" activeStyle>
-           Login
-
-           </NavLink>
-           
-           <NavLink to ="/about" activeStyle>
-            about us
-
-           </NavLink>
-           <NavLink to ="/about" activeStyle>
-          Search
-
-           </NavLink>
-           
-
-           <NavBtn>
-             <NavBtnLink to ="/search"> Register</NavBtnLink>
-             </NavBtn>
-           </NavMenu>
-
-
-         
-       </Nav>
-  </>
-    
-
-
+    <div>Navbar</div>
   )
 }
 

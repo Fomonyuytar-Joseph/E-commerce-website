baaf4c06fdc229ca49e1247abc3c93c99c7fbc92
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2022-04-30 17:26:36 +0100

    init

--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -5,52 +5,60 @@ import im2 from './images/im2.jpg'
 import im3 from './images/im3.jpg'
 
 function Slider() {
-  const [advert ,setAdvert]=useState([])
+  const [adverts ,setAdvert]=useState([
+    {image:im1 , AdvertText:'Get 50% discount of products ' ,buttonText:'Shop Now ',id:1 },
+    {image:im2 , AdvertText:'Get 50% discount of products ' ,buttonText:'Shop Now ' ,id:2},
+    {image:im3, AdvertText:'Get 50% discount of products ' ,buttonText:'Shop Now ', id:3}
+  ])
   return (
-    <div>
-        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+    <>
+    {
+      adverts.map((advert)=>(
+        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
   </div>
   <div class="carousel-inner">
     <div class="carousel-item active">
-<img src={im1} style={ {height: "550px" } }  class="d-block w-100 " alt="..."/>
-    
-<div class="carousel-caption d-none d-md-block">
-        <h1>Get your products here</h1>
-        <p>Some representative placeholder content for the first slide.</p>
-      </div>
-
-    </div>
-    <div class="carousel-item">
-    <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
-      </div>
-
-        
-      <img src={im2} style={ {height: "550px" } }  class="d-block w-100" alt="..."/>
-    </div>
-    <div class="carousel-item">
-      <img src={im3} style={ {height: "550px" } }  class="d-block w-100" alt="..."/>
+      <img src="..." class="d-block w-100" alt="...">
       <div class="carousel-caption d-none d-md-block">
         <h5>First slide label</h5>
         <p>Some representative placeholder content for the first slide.</p>
       </div>
     </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Some representative placeholder content for the second slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Previous</span>
   </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
 </div>
-    </div>
+
+
+      )
+      )
+    }
+    </>
+    
   )
 }
 

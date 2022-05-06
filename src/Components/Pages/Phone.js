6c6bdd189e0453fd6896c8412fad37ee7839234f
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2022-05-06 17:51:59 +0100

    init

--- src/Components/Pages/Phone.js
+++ src/Components/Pages/Phone.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import phone from './images/iphone12.jpg'
 
 function Phone() {
   return (
-    <div>Phone</div>
+    <>
+        <div class="card mb-3" style={{maxWidth: "500px"}}>
+  <div class="row g-0">
+    <div class="col-md-4 " style={{height: "250px"}}>
+      <img src={phone} class="img-fluid rounded-start height-auto pt-5" alt="..."/>
+    </div>
+    <div class="col-md-8 ">
+      <div class="card-body">
+        <h5 class="card-title">iphone 13</h5>
+        <p class="card-text"></p>
+        
+      </div>
+    </div>
+  </div>
+</div>
+
+    </>
   )
 }
 

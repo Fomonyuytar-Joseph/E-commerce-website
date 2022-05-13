cf33abd278b7318c8606b5d5b4ea8c7609d73b71
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2022-05-13 12:59:13 +0100

    init

--- src/Components/Pages/Shoes.js
+++ src/Components/Pages/Shoes.js
@@ -1,8 +1,20 @@
 import React from 'react'
 
 function Shoes() {
+  const [items, setItems] = useState(
+    [  { image:phone ,title:"iphone 13 pro " ,  price:"900,000 FCFA" ,id:1} ,
+      { image:n20 ,title:"Note 20 Ultra" ,  price:"500,000 FCFA",id:2 },
+      { image:i8 ,title:"iphone 8" ,  price:"90,000 FCFA",id:3 },
+      { image:pixel ,title:"Pixel 4a" ,  price:"200,000 FCFA" ,id:4},
+      { image:s21ultra ,title:"Samsung S21 Ultra" ,  price:"700,000 FCFA" ,id:5},
+      { image:oneplus ,title:"Oneplus 8" ,  price:"300,000 FCFA",id:6},
+      { image:p50 ,title:"Huawei P50 Pro" ,  price:"950,000 FCFA" ,id:7},
+      { image:ix ,title:"iphone X" ,  price:"200,000 FCFA",id:8},
+      { image:vivo ,title:"vivo x7 Pro" ,  price:"300,000 FCFA" ,id:9}
+    ]
+    )
   return (
-    <div>Shoes</div>
+    
   )
 }
 

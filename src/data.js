b411be1c6ce943c5b12c9945c5375ebfdfd14d40
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2022-05-23 13:19:29 +0100

    init

--- src/data.js
+++ src/data.js
@@ -144,7 +144,7 @@ const data ={
     ],
 
     shoeProducts:[
-        { image:point ,title:"Regular Shoe " ,  price:"900,000 FCFA" ,id:1} ,
+        { image:point ,title:"Regular Shoe " ,  price:100000,id:1} ,
       { image:point2,title:"Note 20 Ultra" ,  price:"500,000 FCFA",id:2 },
       { image:point3 ,title:"iphone 8" ,  price:"90,000 FCFA",id:3 },
       { image:sch1 ,title:"Pixel 4a" ,  price:"200,000 FCFA" ,id:4},
